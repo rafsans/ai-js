c81d07d5639b96ccb544c4bc55e560cbcb75a0ff
Wahyu Hidayat <wahyuhdyt1515@gmail.com>
2026-06-03 19:01:18 +0700

    Remove comment on class_weight in baseline model
    Removed comment about class_weight usage in the model.

--- 5_train_baseline_tfidf.py
+++ 5_train_baseline_tfidf.py
@@ -103,7 +103,6 @@ def main():
 
     baseline_model = Pipeline([
         ("tfidf", TfidfVectorizer(max_features=10000, ngram_range=(1, 2), min_df=1)),
-        # class_weight='balanced' karena dataset tidak seimbang; tidak menggunakan SMOTE
         ("clf", LogisticRegression(max_iter=1000, class_weight="balanced")),
     ])
 
